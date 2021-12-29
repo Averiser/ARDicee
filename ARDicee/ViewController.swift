9a1c73d6b6c553af01408a1bc38f52385c7ba493
Averiser <averiser@gmail.com>
2021-12-29 16:38:36 +0300

    thirteenth commit - Refactoring part2

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -66,6 +66,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
   
+  //MARK: - Dice Rendering Methods
+  
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     if let touch = touches.first {
       let touchLocation = touch.location(in: sceneView)
@@ -138,33 +140,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
   }
   
+  //MARK: - ARSCNViewDelegateMethods
+  
   func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-    if anchor is ARPlaneAnchor {
-      
-      let planeAnchor = anchor as! ARPlaneAnchor
-      
-      let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-      
-      let planeNode = SCNNode()
-      
-      planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
-      
-      planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
-      
-      let gridMaterial = SCNMaterial()
-      
-      gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
-      
-      plane.materials = [gridMaterial]
-      
-      planeNode.geometry = plane
-      
-      node.addChildNode(planeNode)
-      
-    } else {
-        return
-    }
+    
+    guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+
+    let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+    
+    let planeNode = SCNNode()
+    
+    planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+    
+    planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+    
+    let gridMaterial = SCNMaterial()
+    
+    gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+    
+    plane.materials = [gridMaterial]
+    
+    planeNode.geometry = plane
+    
+    node.addChildNode(planeNode)
+    
   }
 
    
 }
+
+
